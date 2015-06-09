34353185916e09bc04d0e42d6e5ad77983a6df5a
Ramesh Kumar <pchvramesh@gmail.com>
2015-06-09 12:21:46 +0530

    Angular Views

--- ui-nestedState/js/app.js
+++ ui-nestedState/js/app.js
@@ -2,11 +2,27 @@
  * Created by WebApp on 2/25/2015.
  */
 "use strict";
-var TestApp = angular.module("testApp",["ui.router"]).config(["$urlRouterProvider","$stateProvider",function(a,b) {
-    b.state("login", {
-        url: "/login",
-        title: "Login",
-        templateUrl: "views/test.html"
+
+angular.module('testApp', [
+    'ui.router'
+]).config(["$urlRouterProvider","$stateProvider",function(a,b) {
+    a.otherwise('/login')
+    b.state("app", {
+        url: '/login',
+        views: {
+            '@' : {
+                templateUrl: 'views/app.html',
+                controller: 'IndexCtrl'
+            },
+            'brand@app' : { templateUrl: 'views/brand.html'},
+            'login@app' : { templateUrl: 'views/login.html'}
+        }
     })
-    a.otherwise('/login');
-}]);
+    b.state('app.signup',{
+        url:"/signup",
+        'brand@app' : { templateUrl: 'views/brand.html'},
+        'login@app' : { templateUrl: 'views/signup.html'}
+    })
+}]).controller('IndexCtrl',function(){
+    console.log('Index Controller');
+})
